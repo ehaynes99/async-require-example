c349ebfafda41d8a145ec122e950930da2f091d2
Eric Haynes <eric.haynes@openphone.co>
2023-08-13 19:02:04 -0400

    change to callback model

--- example-sources/index.js
+++ example-sources/index.js
@@ -1,17 +1,26 @@
 const file = require('./file.js')
 
+const modules = [] 
+function register(cb) {
+  modules.push(cb())
+}
+
+register(() => require( './foo.js'))
+register(() => require( './bar.js'))
+register(() => require( './baz.js'))
+
 let res = {}
 
 if (file.foo) {
-  res.foo = require( './foo.js')
+  res.foo = modules[0]
 }
 
 if (file.bar) {
-  res.bar = require('./bar.js')
+  res.bar = modules[1]
 }
 
 if (file.baz) {
-  res.baz = require('./baz.js')
+  res.baz = modules[2]
 }
 
 console.log('res', res)
